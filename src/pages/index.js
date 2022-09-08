51dd7e1010dba41339296b24b2c15093ed4fe29a
amorabda <38579797+amorabda@users.noreply.github.com>
2022-09-08 10:40:58 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,13 @@ const IndexPage = () => {
     return <Layout>
         <Hero bg={bgImage}>
         <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5 text-lg">Cloudstream is an Android app for streaming and downloading Movies, TV-Series and Anime.</p>
+            <p className="mb-5 text-lg">InnoFlix is an Android app for streaming and downloading Movies, TV-Series and Anime.</p>
             <div className="flex justify-center w-full mb-5">
                 <CompatBtn autoFocus={true} className="btn-primary" href="/install">Install</CompatBtn>
                 <div className="divider divider-horizontal" />
                 <CompatBtn className="btn-primary" href="/repos">Repositories</CompatBtn>
             </div>
-            <Link to="/docs" className="link">Documentation</Link>
+         
         </Hero>
     </Layout>
 
@@ -30,8 +30,8 @@ export function Head() {
     return (
         <>
             <title>Cloudstream</title>
-            <meta property="og:title" content="Cloudstream" />
-            <meta property="og:description" content="Cloudstream is an Android app for streaming and downloading Movies, TV-Series and Anime." />
+            <meta property="og:title" content="InnoFlix" />
+            <meta property="og:description" content="InnoFlix is an Android app for streaming and downloading Movies, TV-Series and Anime." />
             <meta property="og:image" content={bgImage} />
             <meta property="og:image:type" content="image/png" />
             <meta name="twitter:card" content="summary_large_image" />

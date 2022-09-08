5b0a81bb691f59f13f773f1cfe1cf471638d535c
amorabda <38579797+amorabda@users.noreply.github.com>
2022-09-08 10:46:39 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => (
     <footer class="footer footer-center p-10 mt-15 bg-base-200 text-base-content rounded gap-2">
         <div class="grid grid-flow-col gap-4">
             <a class="link link-hover" href="https://github.com/amorabda/">Github</a>
-            <a class="link link-hover" onClick={() => {window.open("https://www.youtube.com/channel/UCZbbj-dZKkxAXBxNBHJSJ2A")}}>DMCA</a>
+            <a class="link link-hover" onClick={() => {window.open("https://www.youtube.com/channel/UCZbbj-dZKkxAXBxNBHJSJ2A")}}>YouTube</a>
         </div>
         <div>
             <p>Website by <a className="link" href="https://github.com/amorabda" target="_blank">Axinno</a></p>

9cd71208042e0d549914114c1c488f76dfb03864
axinno-cloud <113512902+axinno-cloud@users.noreply.github.com>
2022-09-15 13:18:03 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,7 @@ import React from "react"
 const Footer = () => (
     <footer class="footer footer-center p-10 mt-15 bg-base-200 text-base-content rounded gap-2">
         <div class="grid grid-flow-col gap-4">
-            <a class="link link-hover" href="https://github.com/amorabda/">Github</a>
-            <a class="link link-hover" onClick={() => {window.open("https://www.youtube.com/channel/UCZbbj-dZKkxAXBxNBHJSJ2A")}}>YouTube</a>
+            
         </div>
         <div>
             <p>Website by <a className="link" href="https://axinno.net/" target="_blank">Axinno</a></p>

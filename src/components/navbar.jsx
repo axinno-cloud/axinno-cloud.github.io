4c468d5154710eb2ad0abf9fb234548aa3257679
axinno-cloud <113512902+axinno-cloud@users.noreply.github.com>
2022-09-15 11:44:42 +0200

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,14 +11,10 @@ const Navbar = () => (
 <div className="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur text-primary-content">
     <nav className="navbar w-full">
         <div className="navbar-start flex flex-0 md:gap-1 lg:gap-2">
-            <span className="tooltip tooltip-bottom before:text-xs before:content-[attr(data-tip)]" data-tip="Menu">
-                <label for="drawer" className="btn btn-square btn-ghost drawer-button text-xl text-base-content">
-                    <FaBars />
-                </label>
-            </span>
+            
         </div>
         <div className="flex navbar-center items-center gap-2">
-            <Link className="flex flex-0 btn btn-ghost gap-4 text-base-content" to="/">
+            <Link className="flex flex-0 btn btn-ghost gap-4 text-base-content" to="/repos">
                 <img src={logo} alt="Logo" className="object-contain h-12 w-24" />
                 <span className="hidden md:inline">InnoFlix</span>
             </Link>

c6674172fe9c172ae0c95f0f5826b97688ecf51e
axinno-cloud <113512902+axinno-cloud@users.noreply.github.com>
2022-09-15 10:24:18 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         ]
       }
     },
-    { resolve: 'gatsby-plugin-html-attributes', options: { lang: 'en', 'data-theme': 'dark' } },
-    { resolve: 'gatsby-plugin-nprogress', options: { color: '#6419E6' } },
+    { resolve: 'gatsby-plugin-html-attributes', options: { lang: 'en', 'data-theme': 'night' } },
+    { resolve: 'gatsby-plugin-nprogress', options: { color: '#4ECCA3' } },
     { resolve: 'gatsby-plugin-manifest', options: require("./manifest") },
     { resolve: 'gatsby-plugin-canonical-urls', options: { siteUrl: require("./metadata").siteUrl } },
   ]

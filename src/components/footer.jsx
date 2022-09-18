509f7801bed45da766544d97253b4d4f431c435c
axinno-cloud <113512902+axinno-cloud@users.noreply.github.com>
2022-09-18 17:27:16 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 
 const Footer = () => (
-    <footer class="footer footer-center p-10 mt-15 bg-base-200 text-base-content rounded gap-2">
+    <footer class="footer footer-center p-10 mt-15 bg-base-100 text-base-content rounded gap-2">
         <div class="grid grid-flow-col gap-4">
             
         </div>
         <div>
-            <p>Website by <a className="link" href="https://axinno.net/" target="_blank">Axinno</a></p>
+            
         </div>
     </footer>)
 
